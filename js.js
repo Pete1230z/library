830f4e57db57326ccceed1f0a25eaa7de06c9259
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-08-15 15:07:42 -0700

    Clicking on submit button sends an alert

--- js.js
+++ js.js
@@ -1,17 +1,32 @@
 let myLibrary = []
 
-function book(name, pages, read) {
-	this.name = name
+function Book(title, author, pages, read) {
+	this.title = title
+	this.author = author
 	this.pages = pages
 	this.read = read
 }
 
-function addToLibrary () {
 
+function addToLibrary () {
+	const title = getElementById('title').value
+	const author = getElementById('author').value
+	const pages = getElementById('pages').value
+	const read = getElementById('read').checked
+    
+	const newBook = new Book(title, author, pages, read)
 }
+
+/*Makes form appear when Add Book is clicked on*/
 
 const bookAddition = document.getElementById('addBookButton');
 
 bookAddition.addEventListener('click', function() {
-    addBookDiv.style.display = 'flex'
+    addBookDiv.style.visibility = 'visible'
+	addBookDiv.style.opacity = '1'
+})
+
+document.getElementById('submit').addEventListener('click', function() {
+	event.preventDefault();
+	alert('Hey')
 })
