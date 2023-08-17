a4592f05d30674b72a5ff7926debb2faa5cc7770
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-08-17 09:34:52 -0700

    Successfully can create a div with book information

--- js.js
+++ js.js
@@ -7,6 +7,23 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
+function renderHtml (book) {
+
+	//Gets the shelf id
+	const bookShelf = document.getElementById('shelf');
+
+	//Creates a new div
+	const newDiv = document.createElement('div');
+
+	//Passes in text content from book information passed in the form
+	newDiv.textContent = `${book.title}, ${book.author}, ${book.pages}, ${book.read}`;
+
+	newDiv.className = 'shelf'
+
+	//Appends the new div created to the div with the shelf class
+	bookShelf.appendChild(newDiv);
+}
+
 function addToLibrary() {
 	const title = document.getElementById('title').value;
 	const author = document.getElementById('author').value;
@@ -14,11 +31,12 @@ function addToLibrary() {
 	const read = document.getElementById('read').checked; 
 	const newBook = new Book(title, author, pages, read);
 	myLibrary.push(newBook);
-	render();
+
+	//Sends new book information to the renderHtml function
+	renderHtml(newBook);
 }
 
-/*Makes form appear when Add Book is clicked on*/
-
+//Makes form appear when Add Book is clicked on
 const bookAddition = document.getElementById('addBookButton');
 
 bookAddition.addEventListener('click', function() {
@@ -26,15 +44,16 @@ bookAddition.addEventListener('click', function() {
 	addBookDiv.style.opacity = '1';
 })
 
-/*When anywhere but the form is clicked on it will make the form disappear*/
-
+//When anywhere but the form is clicked on it will make the form disappear
 document.addEventListener('click',function(event) {
      if(!addBookDiv.contains(event.target) && event.target !== bookAddition) { 
 	 addBookDiv.style.visibility = 'hidden';
 	 addBookDiv.style.opacity = '0';
+	 document.querySelectorAll('#formGroup').innerHTML = ''
 	 }
 })
 
+//When form is submitted it prevents information from being sent to backend and calls the addToLibrary function
 document.getElementById('form').addEventListener('submit', function() {
 	event.preventDefault();
 	addToLibrary();
